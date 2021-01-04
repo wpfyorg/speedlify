93e57a6df154c1a663c2e03502ccc5a18ef6704d
Arnab Mohapatra <53978924+wpfyorg@users.noreply.github.com>
2021-01-05 00:35:24 +0530

    Update wpfy.js

--- _data/sites/wpfy.js
+++ _data/sites/wpfy.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: "wpfy.org", // optional, falls back to object key
 	description: "Arnab's Personal Sites",
 	options: {
-		frequency: 60 * 23, // (in minutes), 23 hours
+		frequency: 1 * 5, // (in minutes), 23 hours
 	},
 	urls: [
 		"https://wpfy.org/",

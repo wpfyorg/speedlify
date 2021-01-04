79c633613415becf2fe5ca81d72a3c5ee122ec50
Arnab Mohapatra <53978924+wpfyorg@users.noreply.github.com>
2021-01-05 00:25:24 +0530

    Update zachleat.js

--- _data/sites/zachleat.js
+++ _data/sites/zachleat.js
@@ -13,17 +13,6 @@ module.exports = {
 		freshChrome: "site"
 	},
 	urls: [
-		"https://www.zachleat.com/",
-		"https://www.zachleat.com/web/",
-		"https://www.zachleat.com/web/about/",
-		"https://www.zachleat.com/web/fonts/",
-		"https://www.zachleat.com/web/eleventy/",
-		"https://www.zachleat.com/resume/",
-		"https://www.zachleat.com/twitter/",
-		// Popular Posts
-		"https://www.zachleat.com/web/lighthouse-in-footer/",
-		"https://www.zachleat.com/web/speedlify/",
-		"https://www.zachleat.com/web/comprehensive-webfonts/",
-		"https://www.zachleat.com/web/google-fonts-display/",
+		"https://wpfy.org/adding-a-putty-ppk-key-to-macos-ssh-terminal/",
 	]
 };
